a80d1a3982d1a956152e05e16a5e1ba8f41970df
J909 <J909@users.noreply.github.com>
2018-12-09 00:26:40 -0800

    Adds search of metadata by recording as fallback when no release is found

--- artworker.js
+++ artworker.js
@@ -5,46 +5,76 @@ const template = require('lodash.template');
 const querystring = require('querystring');
 const pkg = require('./package.json');
 
+const metaUrlTemplate =
+    template("https://musicbrainz.org/ws/2/release?query=artist:<%= artist %>%20AND%20<%= table %>:<%= title %>&limit=1&fmt=json");
+const artUrlTemplate =
+    template("http://coverartarchive.org/release/<%= mbid %>/front-1200");
+
 function ArtWorker(userAgentApplication) {
-  this.userAgent = `${userAgentApplication}/${pkg.version}`;
-  this.metaUrlTemplate =
-    template("https://musicbrainz.org/ws/2/release?query=artist:<%= artist %>%20AND%20release:<%= title %>&limit=1&fmt=json");
-	this.artUrlTemplate =
-    template("http://coverartarchive.org/release/<%= mbid %>/front");
   this.metaClient = got.extend({
       json: true,
       headers: {
-        'user-agent': this.userAgent
+        'user-agent': `${userAgentApplication}/${pkg.version}`
       }
     });
 
-  console.log('this.userAgent=' + this.userAgent);
+  this.findArtworkUrl = function(artist, title) {
+    artist = querystring.escape(artist);
+    title = querystring.escape(title);
+    // Search for musicbrainz id for given track meta
+    return this.metaClient.get(searchUrlFrom(artist, title, 'release'))
+      .then(response => {
+        if (!isValidResponse(response)) {
+          throw new Error(unexpectedResponseStatus(response));
+        }
+        if (response.body.releases.length > 0) {
+          return response.body.releases[0].id;
+        }
+        this.metaClient.get(searchUrlFrom(artist, title, 'recording'))
+          .then(response => {
+            if (!isValidResponse(response)) {
+              throw new Error(unexpectedResponseStatus(response));
+            }
+            if (response.body.releases.length > 0) {
+              return response.body.releases[0].id;
+            }
+            return null;
+          });
+      })
+      .then(mbid => {
+        // Compose artwork URI with given musicbrainz id
+        if (mbid) {
+          return artUrlTemplate({ mbid: mbid });
+        }
+        throw new Error('Cannot find metadata');
+      });
+  }
 }
 
-ArtWorker.prototype.findArtworkUrl = function(artist, title) {
-  let metaUrl = this.metaUrlTemplate({
-    artist:querystring.escape(artist),
-    title:querystring.escape(title)
+function searchUrlFrom(artist, title, table) {
+  let url = metaUrlTemplate({
+    'artist': artist,
+    'title': title,
+    'table': table
   });
-  console.log(`GET release: ${metaUrl}`);
-  // Find musicbrainz id for given track meta
-  return this.metaClient.get(metaUrl)
-    .then(response => {
-      if (response.statusCode !== 200 || !response.body || !response.body.releases) {
-        throw new Error(`Unexpected response from ${metaUrl}: ${response.statusCode}: ${response.statusMessage}`);
-      }
-      if (response.body.releases.length > 0) {
-        return response.body.releases[0].id;
-      }
-      throw new Error("No releases found");
-    })
-    .then(mbid => {
-      // Compose artwork URI with given musicbrainz id
-      if (mbid) {
-        return this.artUrlTemplate({ mbid: mbid });
-      }
-      throw new Error('Missing mbid');
-    });
+  console.log(url);
+  return url;
+}
+
+function artUrlFrom(mbid) {
+  let url = artUrlTemplate({
+    'mbid': mbid
+  });
+  console.log(url);
+  return url;
+}
+
+function isValidResponse(response) {
+  return response.statusCode === 200 && response.body && response.body.releases;
+}
+
+function unexpectedResponseStatus(response) {
+  return `Unexpected response: ${response.url}: ${response.statusCode}: ${response.statusMessage}`;
 }
 
 module.exports = ArtWorker;
